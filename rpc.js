d45565b0c145cfaac773b40f05e434943aeb9510
Sayrix <43046854+Sayrix@users.noreply.github.com>
2021-06-27 23:18:27 +0200

    🏎️ v2.0.0

--- rpc.js
+++ rpc.js
@@ -1,13 +1,12 @@
 const rpc = require('discord-rpc');
 
 const config = {
-	discordId: "XXXXXXXX", //Discord Id
 	appId: "XXXXXXXX", //Id of your application
 
 	details: "XXXXXXXX", //Your descriptions
-	largeImageKeyName: "large-key-name", // Key to the large image (https://discord.com/developers/applications -> Rich Presence -> Art Assets)
+	largeImageKeyName: "large-image-key", // Key to the large image (https://discord.com/developers/applications -> Rich Presence -> Art Assets)
 	largeImageText: "XXXXXXXX", //Text when you put your mouse on the large image
-	smallImageKeyName: "small-key-name", // Key to the small image (https://discord.com/developers/applications -> Rich Presence -> Art Assets)
+	smallImageKeyName: "small-image-key", // Key to the small image (https://discord.com/developers/applications -> Rich Presence -> Art Assets)
 	smallImageText: "XXXXXXXX", //Text when you put your mouse on the small image
 
 	buttonOneName: "Button 1", //Name of the button
@@ -16,34 +15,29 @@ const config = {
 	buttonTwoUrl: "https://chilledbot.xyz/freenitro" //URL of the button
 };
 
+rpc.register(config.appId);
 const client = new rpc.Client({transport: 'ipc'});
-
-rpc.register(config.discordId);
 
 client.on('ready', () => {
 	console.log('RPC Started');
-	client.request('SET_ACTIVITY', {
-		pid: 9999,
-		activity: {
-			details: config.details,
-			assets: {
-				large_image: config.largeImageKeyName,
-				large_text: config.largeImageText,
-				small_image: config.smallImageKeyName,
-				small_text: config.smallImageText,
+	client.setActivity({
+		details: config.details,
+		largeImageKey: config.largeImageKeyName,
+		largeImageText: config.largeImageText,
+		smallImageKey: config.smallImageKeyName,
+		smallImageText: config.smallImageText,
+		buttons: [
+			{
+				label: config.buttonOneName,
+				url: config.buttonOneUrl
 			},
-			buttons: [
-				{
-					label: config.buttonOneName,
-					url: config.buttonOneUrl
-				},
-				{
-					label: config.buttonTwoName,
-					url: config.buttonTwoUrl
-				}
-			]
-		}
+			{
+				label: config.buttonTwoName,
+				url: config.buttonTwoUrl
+			}
+		]
 	});
 });
 
-client.login(config.discordId);
+const clientId = config.appId
+client.login({ clientId });
